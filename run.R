b2d425841c51caa0eec48334be5b8e7e3d19ab80
Naluona <125794381+NaLuo12@users.noreply.github.com>
2024-03-23 21:41:31 +0800

    Update run.R

--- run.R
+++ run.R
@@ -22,5 +22,27 @@ library(AutoReference)
 ls("package:AutoReference")
 
 
+##push to fihub
+##in terminal
 
+…or create a new repository on the command line
+https://github.com/NaLuo12/AutoRef2.git
+git remote add origin https://github.com/NaLuo12/AutoRef.git
+git@github.com:NaLuo12/AutoRef2.git
+echo "# AutoRef" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:NaLuo12/AutoRef.git
+git push -u origin main
+
+
+…or push an existing repository from the command line
+git remote add origin git@github.com:NaLuo12/AutoRef.git
+git branch -M main
+git push -u origin main
+
+
+git push origin main
 
